e9449df332e97a77d03d31f5ec66b5eabff7c4b8
Romulo Roriz <romulonmroriz@gmail.com>
2023-04-05 20:18:20 +0200

    Set up folder structure / Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const { colors } = require('tailwindcss/colors');
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: ['class'],
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-inter)', ...fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+};
