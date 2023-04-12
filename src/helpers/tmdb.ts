fec4240b94ae457d3b2e57c99682aea310a3fee1
Romulo Roriz <romulonmroriz@gmail.com>
2023-04-12 16:15:14 +0200

    TMDB fetch helpers

--- src/helpers/tmdb.ts
+++ src/helpers/tmdb.ts
@@ -1,34 +1,58 @@
-import { movieQuerySchema } from '@/lib/tmdb';
+const { env } = process;
 import { MovieQuery } from '@/types/tmdb';
-import { axiosRequest } from './axiosRequest';
+import { z } from 'zod';
 
-async function fetchData(endpoint: string, params?: MovieQuery) {
-	const data = await axiosRequest('get', endpoint, { params });
-	return data;
+export const tmdbKey = env.TMDB_API_KEY as string;
+
+export async function fetchData(
+	endpoint: string,
+	api_key: string,
+	params?: MovieQuery
+) {
+	const url = new URL(`https://api.themoviedb.org/3${endpoint}`);
+	url.search = new URLSearchParams(params as Record<string, string>).toString();
+	url.searchParams.append('api_key', api_key);
+	url.searchParams.append('language', 'en-US');
+	url.searchParams.append('sort_by', 'popularity.desc');
+	const data = await fetch(url.toString());
+	return data.json();
 }
 
+// Movie Query Schema User Preferences
+export const movieQuerySchema = z.object({
+	releaseDateGte: z.string().optional(),
+	releaseDateLte: z.string().optional(),
+	genres: z.array(z.string()).optional(),
+	voteGte: z.number().optional(),
+	voteLte: z.number().optional(),
+});
+
 export function getPopularMovies() {
-	return fetchData('/movie/popular');
+	return fetchData('/movie/popular', tmdbKey);
 }
 
 export function getNowPlayingMovies() {
-	return fetchData('/movie/now_playing');
+	return fetchData('/movie/now_playing', tmdbKey);
 }
 
 export function getTopRatedMovies() {
-	return fetchData('/movie/top_rated');
+	return fetchData('/movie/top_rated', tmdbKey);
+}
+
+export function getUpcomingMovies() {
+	return fetchData('/movie/upcoming', tmdbKey);
 }
 
 export function getGenres() {
-	return fetchData('/genre/movie/list');
+	return fetchData('/genre/movie/list', tmdbKey);
 }
 
 export function getPopularActors() {
-	return fetchData('/person/popular');
+	return fetchData('/person/popular', tmdbKey);
 }
 
 export async function getMovieWithQuery(input?: Partial<MovieQuery>) {
 	const validatedInput = movieQuerySchema.parse(input);
 
-	return fetchData('/discover/movie', validatedInput);
+	return fetchData('/discover/movie', tmdbKey, validatedInput);
 }
