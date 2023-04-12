f2514b6db95ac2cfde4b2f0c3b8af234babc56b7
Romulo Roriz <romulonmroriz@gmail.com>
2023-04-12 16:14:51 +0200

    Providers Componetn

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,21 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
 import type { FC, ReactNode } from 'react';
 
 interface ProvidersProps {
 	children: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 const Providers: FC<ProvidersProps> = ({ children }) => {
-	return <SessionProvider>{children}</SessionProvider>;
+	return (
+		<SessionProvider>
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		</SessionProvider>
+	);
 };
 
 export default Providers;
