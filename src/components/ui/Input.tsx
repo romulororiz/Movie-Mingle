78a6f58d669937ba11e650c506f64ec385f37ff8
Romulo Roriz <romulonmroriz@gmail.com>
2023-04-12 16:14:32 +0200

    Style Input element

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { cn } from '@/utils/cn';
+import { forwardRef } from 'react';
+import Icon from '../Icon';
+import { isMobile } from '@/utils/breakpoints';
+import useWindowSize, { WindowSize } from '@/hooks/useWindowSize';
+
+export interface InputProps
+	extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ className, ...props }, ref) => {
+		const windowSize: WindowSize = useWindowSize();
+
+		return (
+			<div className='relative'>
+				<Icon
+					name='Search'
+					color='#FFF'
+					size={18}
+					className={cn('absolute top-2 right-2.5 md:right-3 z-10')}
+				/>
+				<input
+					className={cn(
+						'flex w-60 h-8 rounded-full border border-accent-default bg-dark-background/60 backdrop-blur-sm truncate pl-4 pr-9 py-4 text-sm placeholder:text-slate-400disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none',
+						className
+					)}
+					ref={ref}
+					{...props}
+				/>
+			</div>
+		);
+	}
+);
+Input.displayName = 'Input';
+
+export { Input };
